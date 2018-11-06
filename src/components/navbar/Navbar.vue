6624ff7daae030c24011e18c84b51eeb6c12587c
Énekes Ábel <enekes.abel@gmail.com>
2018-11-06 23:26:44 +0100

    Navbar updated to point to actual routes

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -3,13 +3,20 @@
              :default-active="$route.path"
              class="el-menu-demo"
              mode="horizontal">
-        <el-menu-item index="/home">Home</el-menu-item>
-        <el-menu-item index="/menu_1">Menu 1</el-menu-item>
-        <el-menu-item index="/menu_2">Menu 2</el-menu-item>
+        <el-menu-item :route="{name: Routes.HOME}"
+                      :index="Routes.HOME">Home
+        </el-menu-item>
+        <el-menu-item :route="{name: Routes.DEVICES}"
+                      :index="Routes.DEVICES">Devices
+        </el-menu-item>
+        <el-menu-item :route="{name: Routes.USERS}"
+                      :index="Routes.USERS">Users
+        </el-menu-item>
         <el-submenu index="2" style="float: right;">
             <template slot="title">Profile</template>
-            <el-menu-item index="/settings">Settings</el-menu-item>
-            <el-menu-item index="/login">Log out</el-menu-item>
+            <el-menu-item :route="{name: Routes.LOGIN}"
+                          :index="Routes.LOGIN">Log out
+            </el-menu-item>
         </el-submenu>
     </el-menu>
 </template>
