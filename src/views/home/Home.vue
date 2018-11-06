08b73ad0fc5696ae0206b2f7bf51fe98dcc663b2
Énekes Ábel <enekes.abel@gmail.com>
2018-11-06 22:45:10 +0100

    app shell moved to App component

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,24 +1,7 @@
 <template>
-    <el-container class="container h-100">
-        <el-header>
-            <el-menu :router="true"
-                     :default-active="$route.path"
-                     class="el-menu-demo"
-                     mode="horizontal">
-                <el-menu-item index="/home">Home</el-menu-item>
-                <el-menu-item index="/menu_1">Menu 1</el-menu-item>
-                <el-menu-item index="/menu_2">Menu 2</el-menu-item>
-                <el-submenu index="2" style="float: right;">
-                    <template slot="title">Profile</template>
-                    <el-menu-item index="/settings">Settings</el-menu-item>
-                    <el-menu-item index="/login">Log out</el-menu-item>
-                </el-submenu>
-            </el-menu>
-        </el-header>
-        <el-main class="h-100">
-            <router-view></router-view>
-        </el-main>
-    </el-container>
+    <div>
+        Home
+    </div>
 </template>
 
 <script lang="ts" src="./Home.ts">
