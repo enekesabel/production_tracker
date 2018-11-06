459744efa2b22cf5719f69b00e3352d70899171f
Énekes Ábel <enekes.abel@gmail.com>
2018-11-06 20:47:48 +0100

    element & bootstrap included

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Element from 'element-ui';
+const locale = require('element-ui/lib/locale/lang/en');
 
 Vue.config.productionTip = false;
+
+// applying plugins
+Vue.use(Element, {locale});
 
 new Vue({
   router,
