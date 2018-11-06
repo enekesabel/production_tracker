6aee04c17aecec0365f92a5c9aa741c49ac33e25
Énekes Ábel <enekes.abel@gmail.com>
2018-11-06 20:48:15 +0100

    Home updated with dummy navigation

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png">
-  </div>
+    <el-container class="container h-100">
+        <el-header>
+            <el-menu :router="true"
+                     :default-active="$route.path"
+                     class="el-menu-demo"
+                     mode="horizontal">
+                <el-menu-item index="/home">Home</el-menu-item>
+                <el-menu-item index="/menu_1">Menu 1</el-menu-item>
+                <el-menu-item index="/menu_2">Menu 2</el-menu-item>
+                <el-submenu index="2" style="float: right;">
+                    <template slot="title">Profile</template>
+                    <el-menu-item index="/settings">Settings</el-menu-item>
+                    <el-menu-item index="/login">Log out</el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-header>
+        <el-main class="h-100">
+            <router-view></router-view>
+        </el-main>
+    </el-container>
 </template>
 
 <script lang="ts" src="./Home.ts">
