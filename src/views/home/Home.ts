96d4eab77880c064b7a3fa1edd0003297442eb1e
Énekes Ábel <enekes.abel@gmail.com>
2018-11-06 20:37:01 +0100

    HelloWorld component removed

--- src/views/home/Home.ts
+++ src/views/home/Home.ts
@@ -1,10 +1,5 @@
 import {Component, Vue} from 'vue-property-decorator';
-import HelloWorld from '../../components/hello_world/HelloWorld';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class Home extends Vue {
 }
