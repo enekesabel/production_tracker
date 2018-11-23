4d25ab74f9dcc64f913cc48a6f6a87470cfb2641
Énekes Ábel <enekes.abel@gmail.com>
2018-11-23 21:49:45 +0100

    Devices page updated, showing devices in a table. dialog to add machiens

--- src/views/devices/Devices.ts
+++ src/views/devices/Devices.ts
@@ -1,5 +1,54 @@
 import {Component, Vue} from 'vue-property-decorator';
+import {Action, State} from 'vuex-class';
+import {Machine} from '@/types/Machine';
+import {ElForm} from 'element-ui/types/form';
 
 @Component
 export default class Devices extends Vue {
+
+  @State
+  private machines: Machine[];
+  @Action
+  private fetchMachines: () => Promise<void>;
+  @Action
+  private createMachine: (machine: Machine) => Promise<void>;
+
+  private dialogVisible: boolean = false;
+  private machineToAdd: Machine = {
+    MachineId: '',
+    MachineName: '',
+  };
+  private rules = {
+    MachineId: [
+      {required: true, message: 'Please add a name', trigger: 'blur'},
+    ],
+    MachineName: [
+      {required: true, message: 'Please add an id', trigger: 'blur'},
+    ],
+  };
+
+  created() {
+    this.fetchMachines();
+  }
+
+  private async validateForm() {
+    await (this.$refs.addMachineForm as ElForm).validate();
+  }
+
+  private async addMachine() {
+    try {
+      await this.validateForm();
+      await this.createMachine(this.machineToAdd);
+      this.closeDialog();
+    } catch (e) {
+    }
+  }
+
+  private openDialog() {
+    this.dialogVisible = true;
+  }
+
+  private closeDialog() {
+    this.dialogVisible = false;
+  }
 }
--- src/views/devices/Devices.vue
+++ src/views/devices/Devices.vue
@@ -1,6 +1,48 @@
 <template>
     <div>
-        Devices
+        <h1>
+            Devices
+        </h1>
+        <el-table
+                :data="machines"
+                style="width: 100%">
+            <el-table-column
+                    prop="MachineName"
+                    label="Machine">
+            </el-table-column>
+            <el-table-column
+                    prop="MachineId"
+                    label="Id">
+            </el-table-column>
+            <div slot="empty" class="py-3">
+                <div>You have no connected devices yet.</div>
+                <el-button type="primary"
+                           class="mt-3"
+                           @click="openDialog">Add device
+                </el-button>
+            </div>
+        </el-table>
+        <el-dialog
+                title="Add device"
+                :visible.sync="dialogVisible"
+                width="30%">
+            <el-form :model="machineToAdd"
+                     ref="addMachineForm"
+                     :rules="rules"
+                     label-width="60px">
+                <el-form-item label="Name" prop="MachineName">
+                    <el-input v-model="machineToAdd.MachineName"></el-input>
+                </el-form-item>
+                <el-form-item label="Id" prop="MachineId">
+                    <el-input v-model="machineToAdd.MachineId"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="closeDialog">Cancel</el-button>
+                <el-button type="primary" @click="addMachine">Add</el-button>
+            </div>
+        </el-dialog>
+
     </div>
 </template>
 
