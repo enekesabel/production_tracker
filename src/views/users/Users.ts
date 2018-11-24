b6a5124c5142bfff419cd745ff02a52b6587fa5e
Énekes Ábel <enekes.abel@gmail.com>
2018-11-24 01:52:15 +0100

    Users view updated with user setting editor feature

--- src/views/users/Users.ts
+++ src/views/users/Users.ts
@@ -1,8 +1,11 @@
-import {Component, Vue} from 'vue-property-decorator';
+import {Component, Vue, Watch} from 'vue-property-decorator';
 import {Action, State} from 'vuex-class';
 import {User} from '@/types/User';
+import UserSettingsDialog from '@/components/user_settings_dialog/UserSettingsDialog';
 
-@Component
+@Component({
+  components: {UserSettingsDialog},
+})
 export default class Users extends Vue {
 
   @State
@@ -10,8 +13,22 @@ export default class Users extends Vue {
   @Action
   private fetchUsers: () => {};
 
+  private settingsDialogVisible: boolean = false;
+  private selectedUser: User = null;
+
   created() {
     this.fetchUsers();
   }
 
+  private openSettings(user: User) {
+    this.selectedUser = user;
+    this.settingsDialogVisible = true;
+  }
+
+  @Watch('settingsDialogVisible')
+  onSettingsDialogVisibilityChange(settingsDialogVisible: boolean) {
+    if (!settingsDialogVisible) {
+      this.selectedUser = null;
+    }
+  }
 }
--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -14,7 +14,23 @@
                     prop="emailAddress"
                     label="Email">
             </el-table-column>
+            <el-table-column
+                    align="center"
+                    label="Operations"
+                    width="180">
+                <template slot-scope="scope">
+                    <el-button type="primary"
+                               icon="el-icon-setting"
+                               title="Settings"
+                               size="mini"
+                               @click="openSettings(scope.row)"
+                               circle></el-button>
+                </template>
+            </el-table-column>
         </el-table>
+        <user-settings-dialog v-if="selectedUser"
+                              :visible.sync="settingsDialogVisible"
+                              :user="selectedUser"></user-settings-dialog>
     </div>
 </template>
 
