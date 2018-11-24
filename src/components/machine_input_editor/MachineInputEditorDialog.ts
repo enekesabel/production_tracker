be514b3933c5c2e77ed2ca908d8b7fae57235278
Énekes Ábel <enekes.abel@gmail.com>
2018-11-24 19:44:43 +0100

    MachineInputEditorDialog created

---
+++ src/components/machine_input_editor/MachineInputEditorDialog.ts
@@ -0,0 +1,70 @@
+import {Component, Model, Prop, Watch} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import {DialogMixin} from '@/mixins/DialogMixin';
+import {MachineInput} from '@/types/MachineInput';
+import {GPIOPin} from '@/enums/GPIOPin';
+import {Form} from 'element-ui';
+
+@Component
+export default class MachineInputEditorDialog extends mixins(DialogMixin) {
+
+  @Model('update:input')
+  @Prop({
+    type: Array,
+    required: true,
+  })
+  private input: MachineInput;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  private title: string;
+
+  @Prop({
+    type: Array,
+    required: true,
+  })
+  private availablePins: GPIOPin[];
+
+  private localInput: MachineInput = null;
+  private rules = {
+    name: [
+      {required: true, message: 'Please provide a name', trigger: 'blur'},
+    ],
+    gPIOPin: [
+      {required: true, message: 'Please select a pin', trigger: 'change'},
+    ],
+  };
+
+  async save() {
+    const form = this.$refs.form as Form;
+    try {
+      await form.validate();
+      this.clearForm();
+      this.$emit('update:input', this.localInput);
+      this.close();
+    } catch (e) {
+
+    }
+  }
+
+  clearForm() {
+    const form = this.$refs.form as Form;
+    form.clearValidate();
+  }
+
+  cancel() {
+    this.clearForm();
+    this.close();
+  }
+
+  @Watch('input', {immediate: true})
+  onInputChanged(input: MachineInput) {
+    if (input === this.localInput) {
+      return;
+    }
+    this.localInput = input;
+  }
+
+}
---
+++ src/components/machine_input_editor/MachineInputEditorDialog.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-dialog :visible.sync="localVisible"
+               :title="title"
+               append-to-body>
+        <el-form v-if="localInput"
+                 :model="localInput"
+                 :rules="rules"
+                 ref="form"
+                 label-width="120px">
+            <el-form-item label="Name" prop="name">
+                <el-input v-model="localInput.name"></el-input>
+            </el-form-item>
+            <el-form-item label="Pin" prop="gPIOPin">
+                <el-select v-model="localInput.gPIOPin" placeholder="Select">
+                    <el-option
+                            v-for="pin in availablePins"
+                            :key="pin"
+                            :label="pin"
+                            :value="pin">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="cancel">Cancel</el-button>
+            <el-button type="primary" @click="save">Save</el-button>
+        </div>
+    </el-dialog>
+</template>
+<script lang="ts" src="./MachineInputEditorDialog.ts">
+</script>
