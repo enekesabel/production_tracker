b581f4751d502fba1502aa4a3445721e6f9e8703
Énekes Ábel <enekes.abel@gmail.com>
2018-11-24 01:51:28 +0100

    UserSettingsDialog created

---
+++ src/components/user_settings_dialog/UserSettingsDialog.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-dialog
+            title="User settings"
+            :visible.sync="localVisible"
+            width="30%">
+        <permission-editor v-model="userPermissionsTypes"></permission-editor>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="close">Cancel</el-button>
+            <el-button type="primary" @click="save">Save</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script lang="ts" src="./UserSettingsDialog.ts">
+</script>
