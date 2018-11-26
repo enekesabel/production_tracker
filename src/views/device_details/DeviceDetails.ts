a7e942cc5fe65e75d66694575833c6c55234aa0a
Énekes Ábel <enekes.abel@gmail.com>
2018-11-27 00:44:59 +0100

    charts shown on device details

--- src/views/device_details/DeviceDetails.ts
+++ src/views/device_details/DeviceDetails.ts
@@ -2,8 +2,14 @@ import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
 import {Machine} from '@/types/Machine';
 import {MachineData} from '@/types/MachineData';
 import {MachineDataApi} from '@/api/MachineDataApi';
+import {Getter} from 'vuex-class';
+import {InputType} from '@/enums/InputType';
+import {InputData} from '@/types/InputData';
+import Diagram from '@/components/diagram/Diagram';
 
-@Component
+@Component({
+  components: {Diagram},
+})
 export default class DeviceDetails extends Vue {
 
   @Prop({
@@ -11,8 +17,35 @@ export default class DeviceDetails extends Vue {
     required: true,
   })
   private deviceId: string;
-  private machine: Machine = null;
+
+  @Getter
+  private getMachineById: (machineId: string) => Machine;
+
   private machineData: MachineData = null;
+
+  private get machine(): Machine {
+    if (!this.deviceId) {
+      return null;
+    }
+    return this.getMachineById(this.deviceId) || null;
+  }
+
+  get inputs(): InputData[] {
+    if (!this.machineData) {
+      return [];
+    }
+    return this.machineData.inputs;
+  }
+
+  get inputTypes(): {[k in InputType]?: number} {
+    return Object.keys(InputType).reduce((p, c: string) => {
+      const value = InputType[c as any as number];
+      if (typeof value === 'number') {
+        return {...p, ...{[c]: value}};
+      }
+      return p;
+    }, {});
+  }
 
   @Watch('deviceId', {immediate: true})
   async onDeviceIdChange(deviceId: string) {
