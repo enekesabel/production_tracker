ee0f60b21bdb538f88f33d09878130a575c38bb8
Énekes Ábel <enekes.abel@gmail.com>
2018-11-27 01:01:45 +0100

    welcome message added on home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        Home
+    <div class="d-flex align-items-center justify-content-center w-100 h-100 my-5">
+        <h1 class="my-5">Welcome to ProductionTracker!</h1>
     </div>
 </template>
 
